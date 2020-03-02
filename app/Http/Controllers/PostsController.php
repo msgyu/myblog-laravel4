e008047b4ab4e8b374bb4a78e4beb391fe2ad4c6
nousi <nousidayo@gmail.com>
2020-03-02 20:13:49 +0900

    $postsを定義

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
     public function index() {
-        return "hello";
+        $posts = Post::all();
+        return view('posts.index', ['posts' => $posts]);
     }
 }
